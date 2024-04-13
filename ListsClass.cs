92e592e948c509436172cdb7f3a03a7724732295
Eduardo Braga <braga.eduardoTL@outlook.com>
2024-04-13 09:06:25 -0300

    Add project files.

---
+++ ListsClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextFlyoutRepro
+{
+    public static class ListsClass
+    {
+        public static ObservableCollection<MyClass> MainWindowlistOfItems { get; set; } = [];
+        public static ObservableCollection<MyClass> ItemsWindowlistOfItems { get; set; } = [];
+    }
+}
